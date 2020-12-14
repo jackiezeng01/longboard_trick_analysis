d33322d40a2aebd12b18d607ecb6eae45748177a
jackiezeng01 <dolphin.pool01@gmail.com>
2020-12-13 20:40:26 -0600

    slapped a 20 point moving avg filter on these bad bois

--- JZ_datacollection/moving_avg.m
+++ JZ_datacollection/moving_avg.m
@@ -1,6 +1,10 @@
 function y = moving_avg(a)
-    y = [a(1) + a(2); a(1:end-2) + a(2:end-1) + a(3:end); a(end-1) + a(end)]/3;
+%     3 pt moving avg
+%     y = [a(1) + a(2); a(1:end-2) + a(2:end-1) + a(3:end); a(end-1) + a(end)]/3;
+%     5 pt moving avg
+    y = movmean(a,20);
 end
+
 % function y = movingavg(x)
 %     y = movmean(x,3, 'Endpoints', 'discard');
 %     % Add end elements
